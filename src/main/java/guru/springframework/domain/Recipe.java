ac9fecc3d7b0ca0a565bd72c1432938ab76b85cf
William Casperson <bill.casperson@mssint.com>
2022-05-28 14:35:22 -0500

    Ch 135. added enum Difficulty to Recipe. closes #5

--- src/main/java/guru/springframework/domain/Recipe.java
+++ src/main/java/guru/springframework/domain/Recipe.java
@@ -16,14 +16,15 @@ public class Recipe {
     private String source;
     private String url;
     private String directions;
-    //todo add
-    //private Difficulty difficulty
 
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "recipe")
     private Set<Ingredient> ingredients;
 
     @Lob
     private Byte[] image;
+
+    @Enumerated(value = EnumType.STRING)
+    private Difficulty difficulty;
 
     @OneToOne(cascade = CascadeType.ALL)
     private Notes notes;
@@ -107,6 +108,14 @@ public class Recipe {
 
     public void setImage(Byte[] image) {
         this.image = image;
+    }
+
+    public Difficulty getDifficulty() {
+        return difficulty;
+    }
+
+    public void setDifficulty(Difficulty difficulty) {
+        this.difficulty = difficulty;
     }
 
     public Notes getNotes() {
