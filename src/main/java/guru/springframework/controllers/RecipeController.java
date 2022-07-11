5c7e20e067ba01b93face7b3ef3b90e1de87d504
William Casperson <bill.casperson@mssint.com>
2022-07-11 17:30:00 -0500

    ch 194. added recipeController, test, and html to display a recipe by Id

--- src/main/java/guru/springframework/controllers/RecipeController.java
+++ src/main/java/guru/springframework/controllers/RecipeController.java
@@ -4,22 +4,22 @@ import guru.springframework.services.RecipeService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Slf4j
 @Controller
-public class IndexController {
+public class RecipeController {
 
     private final RecipeService recipeService;
 
-    public IndexController(RecipeService recipeService) {
+    public RecipeController(RecipeService recipeService) {
         this.recipeService = recipeService;
     }
 
-    @RequestMapping({"", "/", "/index"})
-    public String getIndexPage(Model model){
-        log.debug("indexController calling recipeService");
-        model.addAttribute("recipes", recipeService.getRecipes());
-        return "index";
+    @RequestMapping("/recipe/show/{id}")
+    public String showById(@PathVariable String id, Model model){
+        model.addAttribute("recipe", recipeService.findById(Long.valueOf(id)));
+        return "recipe/show";
     }
 }
